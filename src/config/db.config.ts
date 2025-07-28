6ccbdee32883c5be7c8aa187969c97b03006133c
unknown <manuelowen5324f4e@gmail.com>
2025-07-28 20:31:41 +0300

    added drivers assign

--- src/config/db.config.ts
+++ src/config/db.config.ts
@@ -5,11 +5,22 @@ dotenv.config();
 
 export const typeOrmConfig: TypeOrmModuleOptions = {
   type: 'postgres',
-  host: process.env.DB_HOST,
-  port: parseInt(process.env.DB_PORT ?? '5432', 10),
-  username: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
+  
+  // host: process.env.DB_HOST,
+  // port: process.env.DB_PORT
+  // ? parseInt(process.env.DB_PORT, 10)
+  // : undefined,
+  // username: process.env.DB_USER,
+  // password: process.env.DB_PASSWORD,
+  // database: process.env.DB_DATABASE,
+  url: process.env.DATABASE_URL,
+  ssl: true,
+
+  // autoLoadEntities: true,
+  // entities: [__dirname + '/**/*.entity{.ts,.js}'],
+  // synchronize: process.env.DB_SYNC === 'true' ? true : false,
+  // logging: process.env.DB_LOGGING === 'true' ? true : false,
+  // migrations: [__dirname + '/migrations/**/*{.ts,.js}'],
   autoLoadEntities: true,
   synchronize: false, // Disable temporarily to fix schema issues
   logging: false,
